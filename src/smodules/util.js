c0088c9e266a1466a77cd20c38eaca94db4d755c
smori <smori1983@gmail.com>
2012-06-05 12:01:36 +0900

    added: smodules.util

---
+++ src/smodules/util.js
@@ -0,0 +1,46 @@
+/**
+ * smodules.util.console
+ *
+ * on()
+ * off()
+ * log(message)
+ * time(tag)
+ * timeEnd(tag)
+ */
+smodules.util.console = (function() {
+    var that = {},
+        on   = false;
+
+    that.on = function() {
+        on = true;
+        return that;
+    };
+
+    that.off = function() {
+        on = false;
+        return that;
+    };
+
+    that.log = function(message) {
+        if (on) {
+            console.log(message);
+        }
+        return that;
+    };
+
+    that.time = function(tag) {
+        if (on) {
+            console.time(tag);
+        }
+        return that;
+    };
+
+    that.timeEnd = function(tag) {
+        if (on) {
+            console.timeEnd(tag);
+        }
+        return that;
+    };
+    
+    return that;
+})();
