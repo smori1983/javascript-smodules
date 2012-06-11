2325ea716c4eee077c9ad89ed11af52bd6e26493
smori <smori1983@gmail.com>
2012-06-11 13:41:59 +0900

    modified: browser test files

--- browser-test/smodules/template-test.js
+++ browser-test/smodules/template-test.js
@@ -1,79 +1,84 @@
 module("smodules.template");
 
-asyncTest("appendTo without bindParams", function() {
-    var src = "/javascript-smodules/browser-test/tpl/template-test1.html";
-
-    smodules.template(src, {}).appendTo("#template-test1");
-
-    window.setTimeout(function() {
-        var parentId = $("#template-test1-target").parent().attr("id");
-
-        strictEqual(parentId, "template-test1");
-        start();
-    }, 1500);
-});
-
-asyncTest("insertBefore without bindParams", function() {
-    var src = "/javascript-smodules/browser-test/tpl/template-test2.html";
-
-    smodules.template(src, {}).insertBefore("#template-test2-child");
-
-    window.setTimeout(function() {
-        var nextElementId = $("#template-test2-target").next().attr("id");
-
-        strictEqual(nextElementId, "template-test2-child");
-        start();
-    }, 500);
-});
-
-asyncTest("appendTo with bindParams as non-object", function() {
-    var src  = "/javascript-smodules/browser-test/tpl/template-test3.html";
-    var now  = new Date();
-    var date = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate();
+asyncTest("remote source + appendTo", function() {
+    var src = "/javascript-smodules/browser-test/tpl/appendTo.html";
 
     smodules.template(src, {
-        "date": date
-    }).appendTo("#template-test3");
+        items: [
+            { name: "item1", show: true },
+            { name: "item2", show: false },
+            { name: "item3", show: true }
+        ]
+    }).appendTo("#template-appendTo");
 
     window.setTimeout(function() {
-        strictEqual($("#template-test3-target").text(), date);
+        var li = $("#template-appendTo > ul:first > li");
+
+        strictEqual(li.length, 2);
+        strictEqual($(li[0]).text(), "ITEM1");
+        strictEqual($(li[1]).text(), "ITEM3");
         start();
     }, 500);
 });
 
-asyncTest("appendTo with bindParams as object", function() {
-    var src  = "/javascript-smodules/browser-test/tpl/template-test4.html";
-    var bind = {
-        profile: {
-            name: "hoge",
-            age: 20
+asyncTest("remote source + insertBefore", function() {
+    var src = "/javascript-smodules/browser-test/tpl/insertBefore.html";
+
+    smodules.template(src, {
+        foo: {
+            bar: {
+                hoge: "hoge"
+            }
         }
-    };
+    }).insertBefore("#template-insertBefore-child");
+
+    window.setTimeout(function() {
+        var div = $("#template-insertBefore > div:first"),
+            li  = div.find("ul:first").find("li");
+
+        strictEqual(div.attr("id"), "template-insertBefore-target");
+        strictEqual($(li[0]).text(), "hoge");
+        strictEqual($(li[1]).text(), "default");
+        start();
+    }, 500);
+});
+
+test("text area", function() {
+    var src = "#textarea", target = "#template-textarea";
 
     smodules.template(src, {
-        user: bind
-    }).appendTo("#template-test4");
+        items: [{
+            title: "title1",
+            tags:  ["tag1", "tag2", "tag3"]
+        }, {
+            title: "title2",
+            tags:  []
+        }]
+    }).appendTo("#template-textarea");
 
-    window.setTimeout(function() {
-        strictEqual($("#template-test4-target").text(), "name is hoge and age is 20");
-        start();
-    }, 500);
+    strictEqual($(target).find(".item").length, 2);
 });
 
-asyncTest("appendTo with bindParams as object array", function() {
-    var src  = "/javascript-smodules/browser-test/tpl/template-test5.html";
-    var bind = [ { idx: 0 }, { idx: 1 } ];
+/*
+$(function() {
+var src = "#textarea";
+var content = $(src).val();
+var p =smodules.templateParser.parse(content, src);
 
-    smodules.template(src, bind).appendTo("#template-test5");
-
-    window.setTimeout(function() {
-        strictEqual($("#template-test5-target-0").text(), "0");
-        strictEqual($("#template-test5-target-1").text(), "1");
-        start();
-    }, 500);
+smodules.template("#textarea", {
+    str1: "a",
+    str2: "b",
+    bookmark: {
+        items: [{
+            title: "title1",
+            tags: [ "tag1", "tag2", "tag3" ]
+        },{
+            title: "title2",
+            tags: [ "japan", "usa" ]
+        }]
+    }
+}).get(function(text) {
+    console.log(text);
 });
-
-
-
-
-
+});
+*/
