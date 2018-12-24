27cf89beb6fa17f8c550fb14aad1e517e6a479e5
smori <shinichiro.mori.1983@gmail.com>
2018-12-24 22:53:01 +0900

    [QUnit] Implement beforeEach in data.hash-test.js

--- test/smodules/data.hash-test.js
+++ test/smodules/data.hash-test.js
@@ -1,33 +1,31 @@
-QUnit.module('data.hash');
+QUnit.module('data.hash', {
+  beforeEach: function() {
+    this.hash = smodules.data.hash();
+  },
+});
 
 QUnit.test('add', function(assert) {
-  var hash = smodules.data.hash();
+  this.hash.add('hoge', 'test');
+  assert.strictEqual(this.hash.has('hoge'), true);
+  assert.strictEqual(this.hash.get('hoge'), 'test');
 
-  hash.add('hoge', 'test');
-  assert.strictEqual(hash.has('hoge'), true);
-  assert.strictEqual(hash.get('hoge'), 'test');
-
-  hash.add('hoge', 'hogehoge');
-  assert.strictEqual(hash.get('hoge'), 'hogehoge');
+  this.hash.add('hoge', 'hogehoge');
+  assert.strictEqual(this.hash.get('hoge'), 'hogehoge');
 });
 
 QUnit.test('remove', function(assert) {
-  var hash = smodules.data.hash();
+  this.hash.add('name', 'anonymous');
+  assert.strictEqual(this.hash.has('name'), true);
 
-  hash.add('name', 'anonymous');
-  assert.strictEqual(hash.has('name'), true);
-
-  hash.remove('name');
-  assert.strictEqual(hash.has('name'), false);
-  assert.strictEqual(typeof hash.get('name'), 'undefined');
+  this.hash.remove('name');
+  assert.strictEqual(this.hash.has('name'), false);
+  assert.strictEqual(typeof this.hash.get('name'), 'undefined');
 });
 
 QUnit.test('clear', function(assert) {
-  var hash = smodules.data.hash();
-
-  hash.add('name', 'anonymous');
-  hash.add('mail', 'anonymous@example.com');
-  hash.clear();
-  assert.strictEqual(hash.has('name'), false);
-  assert.strictEqual(hash.has('mail'), false);
+  this.hash.add('name', 'anonymous');
+  this.hash.add('mail', 'anonymous@example.com');
+  this.hash.clear();
+  assert.strictEqual(this.hash.has('name'), false);
+  assert.strictEqual(this.hash.has('mail'), false);
 });
