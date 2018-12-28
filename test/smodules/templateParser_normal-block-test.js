c09be2b723b66f3ccb0866606f307160f323b6f6
smori <shinichiro.mori.1983@gmail.com>
2018-12-29 06:40:37 +0900

    [QUnit] update tests for normal block

--- test/smodules/templateParser_normal-block-test.js
+++ test/smodules/templateParser_normal-block-test.js
@@ -20,7 +20,7 @@ QUnit.test('normal block - plain text', function(assert) {
   assert.strictEqual(this.result[0].expr, 'Hello, world!');
 });
 
-QUnit.test('normal block - html tags', function(assert) {
+QUnit.test('normal block - plain html', function(assert) {
   this.source =
     '<ul>' +
     '<li>one</li>' +
@@ -31,6 +31,19 @@ QUnit.test('normal block - html tags', function(assert) {
   assert.strictEqual(this.result.length, 1);
   assert.strictEqual(this.result[0].type, 'normal');
   assert.strictEqual(this.result[0].expr, '<ul><li>one</li><li>two</li></ul>');
+});
+
+QUnit.test('normal block - plain html broken', function(assert) {
+  this.source =
+    '<ul>' +
+    '<li>one' +
+    '<li>two' +
+    '</div>';
+  this.parse();
+
+  assert.strictEqual(this.result.length, 1);
+  assert.strictEqual(this.result[0].type, 'normal');
+  assert.strictEqual(this.result[0].expr, '<ul><li>one<li>two</div>');
 });
 
 QUnit.test('normal block - literal tag', function(assert) {
