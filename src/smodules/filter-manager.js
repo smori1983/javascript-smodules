0fbb3c172cdf2d1d5a0c93c53babd7974fe1547b
smori <shinichiro.mori.1983@gmail.com>
2021-02-11 02:45:39 +0900

    FilterManager

---
+++ src/smodules/filter-manager.js
@@ -0,0 +1,32 @@
+const Hash = require('./data.hash');
+
+class FilterManager {
+  constructor() {
+    this.filters = new Hash();
+  }
+
+  /**
+   * @param {string} name
+   * @param {function} func
+   */
+  register(name, func) {
+    if (typeof func === 'function') {
+      this.filters.add(name, func);
+    }
+  }
+
+  /**
+   * @param {string} name
+   * @return {function}
+   * @throws Error
+   */
+  get(name) {
+    if (this.filters.has(name)) {
+      return this.filters.get(name);
+    }
+
+    throw new Error('filter "' + name + '" not found');
+  }
+}
+
+module.exports = FilterManager;
