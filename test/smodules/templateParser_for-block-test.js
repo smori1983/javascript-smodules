92e35c6e3f046d926ceb7289fa890f814474c8ac
smori <shinichiro.mori.1983@gmail.com>
2021-02-11 21:01:23 +0900

    Update tests

--- test/smodules/templateParser_for-block-test.js
+++ test/smodules/templateParser_for-block-test.js
@@ -23,9 +23,9 @@ QUnit.test('for block - only value part in dummy variable', function(assert) {
   const block = this.result[0];
 
   assert.strictEqual(block.type, 'for');
-  assert.strictEqual(typeof block.header.tmp_k, 'undefined');
-  assert.strictEqual(block.header.tmp_v, 'item');
-  assert.strictEqual(block.header.keys.join('.'), 'items');
+  assert.strictEqual(typeof block.ctrl.tmp_k, 'undefined');
+  assert.strictEqual(block.ctrl.tmp_v, 'item');
+  assert.strictEqual(block.ctrl.keys.join('.'), 'items');
   assert.strictEqual(block.children.length, 3);
 });
 
@@ -39,9 +39,9 @@ QUnit.test('for block - use index in dummy variable', function(assert) {
   const block = this.result[0];
 
   assert.strictEqual(block.type, 'for');
-  assert.strictEqual(block.header.tmp_k, 'idx');
-  assert.strictEqual(block.header.tmp_v, 'item');
-  assert.strictEqual(block.header.keys.join('.'), 'items');
+  assert.strictEqual(block.ctrl.tmp_k, 'idx');
+  assert.strictEqual(block.ctrl.tmp_v, 'item');
+  assert.strictEqual(block.ctrl.keys.join('.'), 'items');
   assert.strictEqual(block.children.length, 3);
 });
 
@@ -55,9 +55,9 @@ QUnit.test('for block - use index in dummy variable - space before comma', funct
   const block = this.result[0];
 
   assert.strictEqual(block.type, 'for');
-  assert.strictEqual(block.header.tmp_k, 'idx');
-  assert.strictEqual(block.header.tmp_v, 'item');
-  assert.strictEqual(block.header.keys.join('.'), 'items');
+  assert.strictEqual(block.ctrl.tmp_k, 'idx');
+  assert.strictEqual(block.ctrl.tmp_v, 'item');
+  assert.strictEqual(block.ctrl.keys.join('.'), 'items');
   assert.strictEqual(block.children.length, 3);
 });
 
@@ -71,5 +71,5 @@ QUnit.test('for block - variable chain in haystack', function(assert) {
   const block = this.result[0];
 
   assert.strictEqual(block.type, 'for');
-  assert.strictEqual(block.header.keys.join('.'), 'items.key1.key2');
+  assert.strictEqual(block.ctrl.keys.join('.'), 'items.key1.key2');
 });
