ce5557b47a2a93dc9ec3d34c408af8a0875af6f1
smori <shinichiro.mori.1983@gmail.com>
2021-02-13 15:18:48 +0900

    {/literal} -> {endliteral}

--- test/smodules/templateParser_literal-block-test.js
+++ test/smodules/templateParser_literal-block-test.js
@@ -7,14 +7,14 @@ QUnit.module('templateParser', {
 });
 
 QUnit.test('literal block', function (assert) {
-  const src = '<div>{literal}{foo} {left}bar{right} {left}/literal{right} function() {};{/literal}</div>';
+  const src = '<div>{literal}{foo} {left}bar{right} {left}endliteral{right} function() {};{endliteral}</div>';
   const result = this.parser.parse(src);
 
   assert.strictEqual(result.length, 3);
   assert.strictEqual(result[0].type, 'normal');
   assert.strictEqual(result[0].value, '<div>');
   assert.strictEqual(result[1].type, 'literal');
-  assert.strictEqual(result[1].value, '{foo} {bar} {/literal} function() {};');
+  assert.strictEqual(result[1].value, '{foo} {bar} {endliteral} function() {};');
   assert.strictEqual(result[2].type, 'normal');
   assert.strictEqual(result[2].value, '</div>');
 });
