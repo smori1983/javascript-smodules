d97936a4fd15c01f8bced048fa1bca33adc413e0
smori <shinichiro.mori.1983@gmail.com>
2021-02-20 23:11:37 +0900

    Templr.renderCached()

--- src/templr.js
+++ src/templr.js
@@ -1,12 +1,29 @@
 const Evaluator = require('./evaluator');
 const FilterManager = require('./filter-manager');
+const Hash = require('./data.hash');
 const parser = require('./parser');
 
 class Templr {
   constructor() {
+    this._templates = new Hash();
+
     this._filterManager = new FilterManager();
     this._evaluator = new Evaluator(this._filterManager);
     this._parser = parser.init();
+  }
+
+  /**
+   * @param {string} cacheId
+   * @param {string} source
+   */
+  registerCache(cacheId, source) {
+    const ast = this._parser.parse(source);
+
+    this._templates.add(cacheId, ast);
+  }
+
+  clearCache() {
+    this._templates.clear();
   }
 
   /**
@@ -16,6 +33,22 @@ class Templr {
    */
   render(source, param) {
     const ast = this._parser.parse(source);
+
+    return this._evaluator.evaluate(ast, [param]);
+  }
+
+  /**
+   * @param {string} cacheId
+   * @param {Object} param
+   * @return {string}
+   * @throws {Error}
+   */
+  renderCached(cacheId, param) {
+    if (!this._templates.has(cacheId)) {
+      throw new Error('template identified by "' + cacheId + '" not registered.');
+    }
+
+    const ast = this._templates.get(cacheId);
 
     return this._evaluator.evaluate(ast, [param]);
   }
