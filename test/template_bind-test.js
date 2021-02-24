20510bc71b5cca299f25a7e05c4909a38b7f701e
smori <shinichiro.mori.1983@gmail.com>
2021-02-25 00:36:42 +0900

    Remove tests

--- test/template_bind-test.js
+++ test/template_bind-test.js
@@ -6,23 +6,6 @@ QUnit.module('template', {
   },
 });
 
-//QUnit.test('bind - get - embedded source - with callback', function(assert) {
-//  var params = { value: 'hoge' };
-//
-//  this.src = '#template-test-source',
-//  this.template.bind(this.src, params).get(function(output) {
-//    assert.strictEqual(output, '<p>hoge</p>');
-//  });
-//});
-
-//QUnit.test('bind - get - embedded source - without callback', function(assert) {
-//  var params = { value: 'hoge' };
-//
-//  this.src = '#template-test-source',
-//
-//  assert.strictEqual(this.template.bind(this.src, params).get(), '<p>hoge</p>');
-//});
-
 QUnit.test('bind - get - string source - with callback', function (assert) {
   const src = '<p>{$value}</p>';
   const params = {value: 'hoge'};
