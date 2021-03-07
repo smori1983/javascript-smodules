186a8a583531eff8794b641035314aea79c6344d
smori <shinichiro.mori.1983@gmail.com>
2021-03-07 12:43:36 +0900

    Variable

---
+++ src/ast-node/variable.js
@@ -0,0 +1,61 @@
+const AstNode = require('./ast-node');
+
+class Variable extends AstNode {
+  type() {
+    return 'var';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    try {
+      this._process(context.sourceTextManager().lookaheadTextManager());
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    try {
+      const parsed = this._process(context.sourceTextManager());
+
+      return {
+        type: this.type(),
+        keys: parsed.split('.'),
+      };
+    } catch (e) {
+      context.exception(e.message);
+    }
+    return super.parse(context);
+  }
+
+  /**
+   * @param {TextManager} tm
+   * @return {string}
+   * @throws {Error}
+   * @private
+   */
+  _process(tm) {
+    let parsed = tm.next('$');
+
+    while (tm.charMatch(/[\w.]/)) {
+      parsed += tm.next(tm.getChar());
+    }
+
+    if (/^\$$|^\$\.|\.$|\.\./.test(parsed)) {
+      throw new Error('invalid variable expression');
+    }
+
+    return parsed.slice(1);
+  }
+}
+
+module.exports = Variable;
