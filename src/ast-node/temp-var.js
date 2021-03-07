016e1daa5cca2e5f22dabd79f5b30011d2e29dcf
smori <shinichiro.mori.1983@gmail.com>
2021-03-07 11:44:27 +0900

    TempVar

---
+++ src/ast-node/temp-var.js
@@ -0,0 +1,44 @@
+const AstNode = require('./ast-node');
+
+class TempVar extends AstNode {
+  type() {
+    return 'temp_var';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {{expr: string, type: string}}
+   */
+  parse(context) {
+    const expr = this._process(context.sourceTextManager());
+
+    try {
+      return {
+        type: this.type(),
+        expr: expr,
+      };
+    } catch (e) {
+      context.exception(e.message);
+    }
+  }
+
+  /**
+   * @param {TextManager} tm
+   * @private
+   */
+  _process(tm) {
+    let s = tm.next('$');
+
+    while (tm.charMatch(/\w/)) {
+      s += tm.next(tm.getChar());
+    }
+
+    if (s === '$') {
+      throw new Error('tmp variable not found');
+    }
+
+    return s.slice(1);
+  }
+}
+
+module.exports = TempVar;
