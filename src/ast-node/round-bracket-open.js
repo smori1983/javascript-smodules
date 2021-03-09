aa96e2df32d6ee501f00f06b8b604d5018266a41
smori <shinichiro.mori.1983@gmail.com>
2021-03-10 00:10:26 +0900

    RoundBracketOpen

---
+++ src/ast-node/round-bracket-open.js
@@ -0,0 +1,45 @@
+const AstNode = require('./ast-node');
+
+class RoundBracketOpen extends AstNode {
+  type() {
+    return 'roundBracket';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    try {
+      this._process(context.sourceTextManager().lookaheadTextManager());
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    const parsed = this._process(context.sourceTextManager());
+
+    return {
+      type: this.type(),
+      expr: parsed,
+    };
+  }
+
+  /**
+   * @param {TextManager} tm
+   * @return {string}
+   * @private
+   */
+  _process(tm) {
+    return tm.next('(');
+  }
+}
+
+module.exports = RoundBracketOpen;
