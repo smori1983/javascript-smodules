3f4e4021b9886c74589e205c47534fe71c2c9e06
smori <shinichiro.mori.1983@gmail.com>
2021-03-09 21:52:42 +0900

    LiteralClose

---
+++ src/ast-node/literal-close.js
@@ -0,0 +1,48 @@
+const AstNode = require('./ast-node');
+
+class LiteralClose extends AstNode {
+  type() {
+    return 'literal_close';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    try {
+      this._process(context.config(), context.sourceTextManager().lookaheadTextManager());
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    this._process(context.config(), context.sourceTextManager());
+
+    return {
+      type: this.type(),
+    };
+  }
+
+  /**
+   * @param {ParseConfig} config
+   * @param {TextManager} tm
+   * @private
+   */
+  _process(config, tm) {
+    tm.next(config.openDelimiter());
+    tm.skipWhitespace();
+    tm.next('endliteral');
+    tm.skipWhitespace();
+    tm.next(config.closeDelimiter());
+  }
+}
+
+module.exports = LiteralClose;
