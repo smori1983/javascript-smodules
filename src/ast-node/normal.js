28b452e040e16a2ef2c8d0540e7bdae66852ceff
smori <shinichiro.mori.1983@gmail.com>
2021-03-10 00:20:43 +0900

    Normal

---
+++ src/ast-node/normal.js
@@ -0,0 +1,46 @@
+const AstNode = require('./ast-node');
+
+class Normal extends AstNode {
+  type() {
+    return 'normal';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    return true;
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    const config = context.config();
+    const tm = context.sourceTextManager();
+    let value = '';
+
+    while (!tm.eof()) {
+      if (context.read('delimiter_open')) {
+        value += context.parse('delimiter_open').expr;
+      } else if (context.read('delimiter_close')) {
+        value += context.parse('delimiter_close').expr;
+      } else if (tm.charIs(config.openDelimiter())) {
+        break;
+      } else if (tm.charIs(config.closeDelimiter())) {
+        context.exception('syntax error');
+      } else {
+        value += tm.next(tm.getChar());
+      }
+    }
+
+    return {
+      type: 'normal',
+      value: value,
+    };
+  }
+}
+
+module.exports = Normal;
