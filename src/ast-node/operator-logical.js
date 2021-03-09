9b1bbe85e0c22ab491b769461b728e9b891b83a1
smori <shinichiro.mori.1983@gmail.com>
2021-03-09 23:15:33 +0900

    OperatorLogical

---
+++ src/ast-node/operator-logical.js
@@ -0,0 +1,50 @@
+const AstNode = require('./ast-node');
+
+class OperatorLogical extends AstNode {
+  type() {
+    return 'andor';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    try {
+      this._process(context.sourceTextManager().lookaheadTextManager());
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    const matched = this._process(context.sourceTextManager());
+
+    return {
+      type: this.type(),
+      expr: matched,
+    };
+  }
+
+  /**
+   * @param {TextManager} tm
+   * @return {string}
+   * @private
+   */
+  _process(tm) {
+    const regexp = /^(and|or)[^\w]/;
+    const matched = tm.regexpMatched(regexp, '"and" or "or" should be written');
+
+    tm.next(matched[1]);
+
+    return matched[1];
+  }
+}
+
+module.exports = OperatorLogical;
