37e2819a88e04bb871b5055e174acd52b1fce651
smori <shinichiro.mori.1983@gmail.com>
2021-03-09 21:43:30 +0900

    LiteralOpen

---
+++ src/ast-node/literal-open.js
@@ -0,0 +1,48 @@
+const AstNode = require('./ast-node');
+
+class LiteralOpen extends AstNode {
+  type() {
+    return 'literal_open';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    try {
+      this._process(context.config(), context.sourceTextManager().lookaheadTextManager());
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    this._process(context.config(), context.sourceTextManager());
+
+    return {
+      type: this.type(),
+    };
+  }
+
+  /**
+   * @param {ParseConfig} config
+   * @param {TextManager} tm
+   * @private
+   */
+  _process(config, tm) {
+    tm.next(config.openDelimiter());
+    tm.skipWhitespace();
+    tm.next('literal');
+    tm.skipWhitespace();
+    tm.next(config.closeDelimiter());
+  }
+}
+
+module.exports = LiteralOpen;
