942c1e50921ec8db3c059f38becd5529657d31df
smori <shinichiro.mori.1983@gmail.com>
2021-03-12 01:09:03 +0900

    FilterChain

---
+++ src/ast-node/filter-chain.js
@@ -0,0 +1,51 @@
+const AstNode = require('./ast-node');
+
+class FilterChain extends AstNode {
+  type() {
+    return 'filter_chain';
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {boolean}
+   */
+  read(context) {
+    return true;
+  }
+
+  /**
+   * @param {ParseContext} context
+   * @return {AstNodeParseResult}
+   */
+  parse(context) {
+    const config = context.config();
+    const tm = context.sourceTextManager();
+    const filters = [];
+
+    while (!tm.eof()) {
+      tm.skipWhitespace();
+
+      if (!tm.charIs('|')) {
+        break;
+      }
+
+      tm.next('|');
+      tm.skipWhitespace();
+
+      filters.push(context.parse('filter'));
+
+      if (tm.charIs(config.closeDelimiter())) {
+        break;
+      } else if (!tm.charIs('|')) {
+        throw new Error('syntax error');
+      }
+    }
+
+    return {
+      type: this.type(),
+      filters: filters,
+    };
+  }
+}
+
+module.exports = FilterChain;
