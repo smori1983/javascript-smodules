0c8b90d6cdeede24c33e98be12c6a268a8bf7aa5
smori <shinichiro.mori.1983@gmail.com>
2021-03-20 01:40:54 +0900

    Modify main loop code of FilterChain.parse()

--- src/ast-node/filter-chain.js
+++ src/ast-node/filter-chain.js
@@ -22,17 +22,13 @@ class FilterChain extends AstNode {
     const tm = context.sourceTextManager();
     const filters = [];
 
-    while (!tm.eof()) {
-      tm.whitespace();
+    tm.whitespace();
 
-      if (!tm.charIs('|')) {
-        break;
-      }
-
+    while (tm.charIs('|')) {
       tm.next('|');
       tm.whitespace();
-
       filters.push(context.parse('filter'));
+      tm.whitespace();
 
       if (tm.charIs(config.closeDelimiter())) {
         break;
