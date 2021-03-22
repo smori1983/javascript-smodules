34884d7c73b405533d4b490a48ad31b179243a95
smori <shinichiro.mori.1983@gmail.com>
2021-03-22 20:01:15 +0900

    Refactor TempVar

--- src/ast-node/temp-var.js
+++ src/ast-node/temp-var.js
@@ -10,32 +10,19 @@ class TempVar extends AstNode {
    * @return {AstNodeParseResult}
    */
   parse(context) {
-    try {
-      const expr = this._consume(context.config(), context.sourceTextManager());
+    const tm = context.sourceTextManager();
 
-      return {
-        type: this.type(),
-        expr: expr,
-      };
-    } catch (e) {
-      context.exception(e.message);
-    }
-  }
-
-  /**
-   * @param {ParseConfig} config
-   * @param {TextManager} tm
-   * @return {string}
-   * @private
-   */
-  _consume(config, tm) {
+    tm.whitespace();
     const s = tm.next('$') + tm.consumeWhile(/\w/);
 
     if (s === '$') {
       throw new Error('tmp variable not found');
     }
 
-    return s.slice(1);
+    return {
+      type: this.type(),
+      expr: s.slice(1),
+    };
   }
 }
 
