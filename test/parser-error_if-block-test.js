328c9093886ddd202605fa489bfae600bbd91c25
smori <shinichiro.mori.1983@gmail.com>
2021-03-26 00:37:13 +0900

    Modify test names

--- test/parser-error_if-block-test.js
+++ test/parser-error_if-block-test.js
@@ -12,132 +12,132 @@ const parseError = (assert, text) => {
   }, Error);
 };
 
-QUnit.module('parser - error');
+QUnit.module('parser - if - error');
 
-QUnit.test('if block - if elseif else - error 1', (assert) => {
+QUnit.test('if elseif else - 1', (assert) => {
   parseError(assert, '{if $foo}<p>foo</p>');
 });
 
-QUnit.test('if block - if elseif else - error 2', (assert) => {
+QUnit.test('if elseif else - 2', (assert) => {
   parseError(assert, '{elseif $foo}<p>foo</p>{endif}');
 });
 
-QUnit.test('if block - if elseif else - error 3', (assert) => {
+QUnit.test('if elseif else - 3', (assert) => {
   parseError(assert, '{else}<p>foo</p>{endif}');
 });
 
-QUnit.test('if block - if elseif else - error 4', (assert) => {
+QUnit.test('if elseif else - 4', (assert) => {
   parseError(assert, '{endif}');
 });
 
-QUnit.test('if block - if elseif else - error 5', (assert) => {
+QUnit.test('if elseif else - 5', (assert) => {
   parseError(assert, '{if $foo}<p>foo</p>{if $bar}<p>bar</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - no space 1', (assert) => {
+QUnit.test('condition - no space 1', (assert) => {
   parseError(assert, '{if$foo }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - no space 2', (assert) => {
+QUnit.test('condition - no space 2', (assert) => {
   parseError(assert, '{if $foo }<p>foo</p>{elseif$bar}<p>bar</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round bracket balance', (assert) => {
+QUnit.test('condition - round bracket balance', (assert) => {
   parseError(assert, '{if ($foo) and $bar) }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - lack of round_bracket_close', (assert) => {
+QUnit.test('condition - lack of round_bracket_close', (assert) => {
   parseError(assert, '{if ( $foo }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - lack of round_bracket_open', (assert) => {
+QUnit.test('condition - lack of round_bracket_open', (assert) => {
   parseError(assert, '{if ( $foo ) ) }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_open -> round_bracket_close', (assert) => {
+QUnit.test('condition - round_bracket_open -> round_bracket_close', (assert) => {
   parseError(assert, '{if () }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_open -> comp', (assert) => {
+QUnit.test('condition - round_bracket_open -> comp', (assert) => {
   parseError(assert, '{if ( === $foo )}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_open -> andor', (assert) => {
+QUnit.test('condition - round_bracket_open -> andor', (assert) => {
   parseError(assert, '{if ( and $foo )}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_close -> round_bracket_open', (assert) => {
+QUnit.test('condition - round_bracket_close -> round_bracket_open', (assert) => {
   parseError(assert, '{if ( $foo ) ( $bar )}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_close -> value', (assert) => {
+QUnit.test('condition - round_bracket_close -> value', (assert) => {
   parseError(assert, '{if ( $foo ) 10 === $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_close -> var', (assert) => {
+QUnit.test('condition - round_bracket_close -> var', (assert) => {
   parseError(assert, '{if ( $foo ) $bar gte 10}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - round_bracket_close -> comp', (assert) => {
+QUnit.test('condition - round_bracket_close -> comp', (assert) => {
   parseError(assert, '{if ( $foo ) === $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - value -> round_bracket_open', (assert) => {
+QUnit.test('condition - value -> round_bracket_open', (assert) => {
   parseError(assert, '{if 10 ( $foo === 1 )}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - value -> value', (assert) => {
+QUnit.test('condition - value -> value', (assert) => {
   parseError(assert, '{if 10 20}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - value -> var', (assert) => {
+QUnit.test('condition - value -> var', (assert) => {
   parseError(assert, '{if 10 $foo}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - var -> round_bracket_open', (assert) => {
+QUnit.test('condition - var -> round_bracket_open', (assert) => {
   parseError(assert, '{if $foo ( $bar === 1 )}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - var -> value', (assert) => {
+QUnit.test('condition - var -> value', (assert) => {
   parseError(assert, '{if $foo 10}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - var -> var', (assert) => {
+QUnit.test('condition - var -> var', (assert) => {
   parseError(assert, '{if $foo $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> round_bracket_open', (assert) => {
+QUnit.test('condition - comp -> round_bracket_open', (assert) => {
   parseError(assert, '{if $foo lte ( $bar ) }<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> round_bracket_close', (assert) => {
+QUnit.test('condition - comp -> round_bracket_close', (assert) => {
   parseError(assert, '{if ( $foo gte ) $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> comp', (assert) => {
+QUnit.test('condition - comp -> comp', (assert) => {
   parseError(assert, '{if $foo === === $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> andor', (assert) => {
+QUnit.test('condition - comp -> andor', (assert) => {
   parseError(assert, '{if $foo !== or $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> value -> comp', (assert) => {
+QUnit.test('condition - comp -> value -> comp', (assert) => {
   parseError(assert, '{if 10 === 10 === 10}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - comp -> var -> comp', (assert) => {
+QUnit.test('condition - comp -> var -> comp', (assert) => {
   parseError(assert, '{if $foo gt $bar gt $baz}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - andor -> round_bracket_close', (assert) => {
+QUnit.test('condition - andor -> round_bracket_close', (assert) => {
   parseError(assert, '{if ( $foo or ) $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - andor -> comp', (assert) => {
+QUnit.test('condition - andor -> comp', (assert) => {
   parseError(assert, '{if $foo or === $bar}<p>ok</p>{endif}');
 });
 
-QUnit.test('if block - conditions - error - andor -> andor', (assert) => {
+QUnit.test('condition - andor -> andor', (assert) => {
   parseError(assert, '{if $foo or or $bar}<p>ok</p>{endif}');
 });
