13fb2b8815007814d15dde9b046638be3b6004fc
smori <shinichiro.mori.1983@gmail.com>
2021-03-29 00:05:21 +0900

    Arrange normal block parse tests

--- test/parser-error_normal-block-test.js
+++ test/parser-error_normal-block-test.js
@@ -1,23 +1,23 @@
 const Parser = require('../src/parser');
 
-QUnit.module('parser - error', {
-  beforeEach: function () {
-    this.parser = new Parser();
-  },
+/**
+ * @param {Assert} assert
+ * @param {string} text
+ */
+const parseError = (assert, text) => {
+  const parser = new Parser();
+
+  assert.throws(() => {
+    parser.parse(text);
+  }, Error);
+};
+
+QUnit.module('parser - normal - error');
+
+QUnit.test('1', (assert) => {
+  parseError(assert, '<div>{left} is ok, only { is forbidden.</div>');
 });
 
-QUnit.test('normal block - error - 1', function (assert) {
-  const src = '<div>{left} is ok, only { is forbidden.</div>';
-
-  assert.throws(function () {
-    this.parser.parse(src);
-  }, Error);
-});
-
-QUnit.test('normal block - error - 2', function (assert) {
-  const src = '<div> } is forbidden.</div>';
-
-  assert.throws(function () {
-    this.parser.parse(src);
-  }, Error);
+QUnit.test('2', (assert) => {
+  parseError(assert, '<div> } is forbidden.</div>');
 });
