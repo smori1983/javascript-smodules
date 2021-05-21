6a8ebf73b585d4b0053d3a6b0792bc63b56d5b91
smori <shinichiro.mori.1983@gmail.com>
2021-05-22 01:05:46 +0900

    [test] rewrite with mocha

--- test/ast-node/value.js
+++ test/ast-node/value.js
@@ -1,65 +1,68 @@
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('assert');
 const contextBuilder = require('../../test_lib/parse-context-builder');
 
-QUnit.module('ast-node - value - bool');
+describe('ast-node', () => {
+  describe('value - bool', () => {
+    describe('read - error', () => {
+      it('empty string', () => {
+        const context = contextBuilder.build('');
+        assert.deepStrictEqual(context.read('value_bool'), false);
+      });
 
-QUnit.test('read - error - empty string', function (assert) {
-  const context = contextBuilder.build('');
+      it('uppercase', () => {
+        const context = contextBuilder.build('TRUE');
+        assert.deepStrictEqual(context.read('value_bool'), false);
+      });
+    });
 
-  assert.deepEqual(context.read('value_bool'), false);
-});
+    describe('read - ok', () => {
+      it('space', () => {
+        const context = contextBuilder.build(' true ');
+        assert.deepStrictEqual(context.read('value_bool'), true);
+      });
 
-QUnit.test('read - error - uppercase', function (assert) {
-  const context = contextBuilder.build('TRUE');
+      it('operator no space - 1', () => {
+        const context = contextBuilder.build('true===true');
+        assert.deepStrictEqual(context.read('value_bool'), true);
+      });
 
-  assert.deepEqual(context.read('value_bool'), false);
-});
+      it('operator no space - 2', () => {
+        const context = contextBuilder.build('true!==true');
+        assert.deepStrictEqual(context.read('value_bool'), true);
+      });
+    });
+  });
 
-QUnit.test('read - ok - space', function (assert) {
-  const context = contextBuilder.build(' true ');
+  describe('value - null', () => {
+    describe('read - error', () => {
+      it('empty string', () => {
+        const context = contextBuilder.build('');
+        assert.deepStrictEqual(context.read('value_null'), false);
+      });
 
-  assert.deepEqual(context.read('value_bool'), true);
-});
+      it('uppercase', () => {
+        const context = contextBuilder.build('NULL');
+        assert.deepStrictEqual(context.read('value_null'), false);
+      });
+    });
 
-QUnit.test('read - ok - operator no space - 1', function (assert) {
-  const context = contextBuilder.build('true===true');
+    describe('read - ok', () => {
+      it('space', () => {
+        const context = contextBuilder.build(' null ');
+        assert.deepStrictEqual(context.read('value_null'), true);
+      });
 
-  assert.deepEqual(context.read('value_bool'), true);
-});
+      it('operator no space - 1', () => {
+        const context = contextBuilder.build('null===true');
+        assert.deepStrictEqual(context.read('value_null'), true);
+      });
 
-QUnit.test('read - ok - operator no space - 2', function (assert) {
-  const context = contextBuilder.build('true!==true');
-
-  assert.deepEqual(context.read('value_bool'), true);
-});
-
-QUnit.module('ast-node - value - null');
-
-QUnit.test('read - error - empty string', function (assert) {
-  const context = contextBuilder.build('');
-
-  assert.deepEqual(context.read('value_null'), false);
-});
-
-QUnit.test('read - error - uppercase', function (assert) {
-  const context = contextBuilder.build('NULL');
-
-  assert.deepEqual(context.read('value_null'), false);
-});
-
-QUnit.test('read - ok - space', function (assert) {
-  const context = contextBuilder.build(' null ');
-
-  assert.deepEqual(context.read('value_null'), true);
-});
-
-QUnit.test('read - ok - operator no space - 1', function (assert) {
-  const context = contextBuilder.build('null===true');
-
-  assert.deepEqual(context.read('value_null'), true);
-});
-
-QUnit.test('read - ok - operator no space - 2', function (assert) {
-  const context = contextBuilder.build('null!==true');
-
-  assert.deepEqual(context.read('value_null'), true);
+      it('operator no space - 2', () => {
+        const context = contextBuilder.build('null!==true');
+        assert.deepStrictEqual(context.read('value_null'), true);
+      });
+    });
+  });
 });
