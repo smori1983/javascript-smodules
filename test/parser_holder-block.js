f6d3fb198a0c00463a23b45730dfc068c9cb7a18
smori <shinichiro.mori.1983@gmail.com>
2021-08-01 23:59:28 +0900

    [test] rewrite parser_holder-block-test.js

---
+++ test/parser_holder-block.js
@@ -0,0 +1,79 @@
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('assert');
+const Parser = require('../src/parser');
+
+/**
+ * @param {string} text
+ * @return {AstNodeParseResult[]}
+ */
+const parse = (text) => {
+  const parser = new Parser();
+
+  return parser.parse(text);
+};
+
+describe('Parser - holder', () => {
+  it('no filters', () => {
+    const text = '{ $foo.bar }';
+    const result = parse(text);
+
+    assert.strictEqual(result.length, 1);
+    assert.strictEqual(result[0].type, 'holder');
+    assert.strictEqual(result[0].keys.length, 2);
+    assert.strictEqual(result[0].keys[0], 'foo');
+    assert.strictEqual(result[0].keys[1], 'bar');
+    assert.strictEqual(result[0].filters.length, 0);
+  });
+
+  it('filters with no args', () => {
+    const text = '{ $foo | filter1 | filter2 }';
+    const result = parse(text);
+
+    const filter1 = result[0].filters[0];
+    const filter2 = result[0].filters[1];
+
+    assert.strictEqual(filter1.name, 'filter1');
+    assert.strictEqual(filter1.args.length, 0);
+    assert.strictEqual(filter2.name, 'filter2');
+    assert.strictEqual(filter2.args.length, 0);
+  });
+
+  it('filter with args - null, true and false', () => {
+    const text = '{ $foo | filter : null, true, false }';
+    const result = parse(text);
+
+    const filter = result[0].filters[0];
+
+    assert.strictEqual(filter.args[0], null);
+    assert.strictEqual(filter.args[1], true);
+    assert.strictEqual(filter.args[2], false);
+  });
+
+  it('filter with args - string', () => {
+    const text = '{ $foo | filter : "test", "{delimiter}", "it\'s string" }';
+    const result = parse(text);
+
+    const filter = result[0].filters[0];
+
+    assert.strictEqual(filter.args[0], 'test');
+    assert.strictEqual(filter.args[1], '{delimiter}');
+    assert.strictEqual(filter.args[2], 'it\'s string');
+  });
+
+  it('filter with args - number', () => {
+    const text = '{ $foo | filter : 0, 10, -99, 12.3, -0.123, 1e+1, 1e1, 10e-1 }';
+    const result = parse(text);
+
+    const filter = result[0].filters[0];
+
+    assert.strictEqual(filter.args[0], 0);
+    assert.strictEqual(filter.args[1], 10);
+    assert.strictEqual(filter.args[2], -99);
+    assert.strictEqual(filter.args[3], 12.3);
+    assert.strictEqual(filter.args[4], -0.123);
+    assert.strictEqual(filter.args[5], 10);
+    assert.strictEqual(filter.args[6], 10);
+    assert.strictEqual(filter.args[7], 1);
+  });
+});
